6f8803625f1c066869a91d0fb1e56069ddf6c88d
oisgoon <oinse719gmail.com>
2024-09-06 13:40:25 +0900

    Spring Initializr 이후 기본 세팅 완료

---
+++ src/main/java/com/example/gong_gam/GongGamApplication.java
@@ -0,0 +1,13 @@
+package com.example.gong_gam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongGamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongGamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gong_gam/GongGamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gong_gam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongGamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
