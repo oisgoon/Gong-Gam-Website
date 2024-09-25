741a3ada4fcbb7fc897d01552794a5807bb3a2df
oisgoon <oinse719@gmail.com>
2024-09-26 00:08:41 +0900

    조회수 2씩 올라가는 오류 수정

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -3,9 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
+// React.StrictMode를 제거합니다.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <App />
 );
